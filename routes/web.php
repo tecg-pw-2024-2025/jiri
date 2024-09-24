0efe7f5974bfd1ad4da914edd9c9aec7f5e1093b
Dominique Vilain <dominique.vilain@hepl.be>
2024-09-24 10:16:31 +0200

    Update jiri routes
    Just a bit of reordering, and planning of actions to handle in the future

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,16 @@
 use App\Http\Controllers\JiriController;
 use Illuminate\Support\Facades\Route;
 
+//Home
 Route::get('/', [JiriController::class, 'index'])->name('home');
+
+//Jiris
+//Read
 Route::get('/jiris', [JiriController::class, 'index'])->name('jiris.index');
-Route::post('/jiris', [JiriController::class, 'store'])->name('jiris.store');
 Route::get('/jiris/create', [JiriController::class, 'create'])->name('jiris.create');
 Route::get('/jiris/{jiri}', [JiriController::class, 'show'])->name('jiris.show');
+Route::get('/jiris/{jiri}/edit', [JiriController::class, 'edit'])->name('jiris.edit');
+//Create, Update, Delete
+Route::post('/jiris', [JiriController::class, 'store'])->name('jiris.store');
+Route::patch('/jiris/{jiri}', [JiriController::class, 'update'])->name('jiris.update');
+Route::delete('/jiris/{jiri}', [JiriController::class, 'destroy'])->name('jiris.destroy');
