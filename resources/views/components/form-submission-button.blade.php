d16a220367ca3e33ba0f13da3a2d8f75b4b8dcd3
Dominique Vilain <dominique.vilain@hepl.be>
2024-09-24 10:24:12 +0200

    fix name of button component

---
+++ resources/views/components/form-submission-button.blade.php
@@ -0,0 +1,4 @@
+<button type="submit"
+        {{ $attributes->class(['font-bold text-white rounded-md p-2 px-4 tracking-wider uppercase']) }}>
+    {{ $slot }}
+</button>
--- resources/views/components/form-sumission-button.blade.php
+++
@@ -1,4 +0,0 @@
-<button type="submit"
-        {{ $attributes->class(['bg-blue-500 font-bold text-white rounded-md p-2 px-4 tracking-wider uppercase']) }}>
-    {{ $slot }}
-</button>
