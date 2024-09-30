13498ed3b9157d027b148689f7559c7c66dc3fa8
Dominique Vilain <dominique.vilain@hepl.be>
2024-09-30 15:51:41 +0200

    Add jiris routes tests for auth user and for guest user

--- tests/Feature/Routing/JiriCrudRoutesTest.php
+++ tests/Feature/Routing/JiriCrudRoutesTest.php
@@ -1,64 +1,58 @@
 <?php
 
 use App\Models\Jiri;
+use App\Models\User;
 
 use function Pest\Laravel\{assertDatabaseHas, assertDatabaseMissing, delete, get, patch, post};
 
+beforeEach(function () {
+    $this->user = User::factory()->create();
+    $this->actingAs($this->user);
+});
+
 // The GET routes
-it('has a route to display jiris', function () {
-    $response = get(route('jiris.index'));
-
-    $response->assertStatus(200);
+// Index
+it('has a route to display jiris to auth users', function () {
+    get(route('jiris.index'))->assertStatus(200);
 });
 
-it('has a route to display one jiri', function () {
+// Show
+it('has a route to display one jiri to auth users', function () {
     $jiri = Jiri::factory()->create();
 
-    $response = get(route('jiris.show', $jiri));
-
-    $response->assertStatus(200);
+    get(route('jiris.show', $jiri))->assertStatus(200);
 });
 
-it('has a route to display a creation form for a jiri', function () {
-    $response = get(route('jiris.create'));
-
-    $response->assertStatus(200);
+it('has a route to display a creation form for a jiri to auth users', function () {
+    get(route('jiris.create'))->assertStatus(200);
 });
 
-it('has a route to display an edit form for a jiri', function () {
+it('has a route to display an edit form for a jiri to auth users', function () {
     $jiri = Jiri::factory()->create();
 
-    $response = get(route('jiris.create', $jiri));
-
-    $response->assertStatus(200);
+    get(route('jiris.create', $jiri))->assertStatus(200);
 });
 
 // The routes changing the database
-it('has a route to store a jiri', function () {
+it('has a route to store a jiri to auth users', function () {
     $jiri = Jiri::factory()->make()->toArray();
 
-    $response = post(route('jiris.store'), $jiri);
-
-    $response->assertStatus(302);
+    post(route('jiris.store'), $jiri)->assertStatus(302);
     assertDatabaseHas('jiris', $jiri);
 });
 
-it('has a route to update a jiri', function () {
+it('has a route to update a jiri to auth users', function () {
     $jiri = Jiri::factory()->create();
 
     $jiri->name = 'Updated name';
 
-    $response = patch(route('jiris.update', $jiri), $jiri->toArray());
-
-    $response->assertStatus(302);
+    patch(route('jiris.update', $jiri), $jiri->toArray())->assertStatus(302);
     assertDatabaseHas('jiris', $jiri->toArray());
 });
 
-it('has a route to delete a jiri', function () {
+it('has a route to delete a jiri to auth users', function () {
     $jiri = Jiri::factory()->create();
 
-    $response = delete(route('jiris.destroy', $jiri));
-
-    $response->assertStatus(302);
+    delete(route('jiris.destroy', $jiri))->assertStatus(302);
     assertDatabaseMissing('jiris', $jiri->toArray());
 });
