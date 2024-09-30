09676c48b87fe8fe4d4057c7babb260f4720ccc8
Dominique Vilain <dominique.vilain@hepl.be>
2024-09-30 17:09:28 +0200

    Update seed to use the relation between user and jiri

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Contact;
-use App\Models\Jiri;
 use App\Models\Project;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -17,8 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
-        Jiri::factory(10)->create();
+        User::factory(10)
+            ->hasJiris(20)
+            ->create();
+        User::factory()
+            ->hasJiris(20)
+            ->create(['email' => 'dominique.vilain@hepl.be']);
+
         Contact::factory(10)->create();
         Project::factory(10)->create();
     }
