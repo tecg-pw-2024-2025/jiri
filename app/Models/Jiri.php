f4582fae73550ecb71e4d8eab3cab4d4ac9219db
Dominique Vilain <dominique.vilain@hepl.be>
2024-09-30 17:09:04 +0200

    Add user relation to jiri model and define foreign key

--- app/Models/Jiri.php
+++ app/Models/Jiri.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Jiri extends Model
 {
@@ -19,4 +20,9 @@ class Jiri extends Model
         'name',
         'starting_at',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
