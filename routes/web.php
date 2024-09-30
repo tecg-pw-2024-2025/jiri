ba174ad57331e5b80cfb7e0749bbee6d1708aa26
Dominique Vilain <dominique.vilain@hepl.be>
2024-09-30 19:11:44 +0200

    remove references to dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+/*Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');*/
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
