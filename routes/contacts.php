2f1efbbb79319a3370d95545387a0e2eaeabf252
Dominique Vilain <dominique.vilain@hepl.be>
2024-09-30 15:29:19 +0200

    Add auth middleware to crud routes

--- routes/contacts.php
+++ routes/contacts.php
@@ -1,13 +1,17 @@
 <?php
+
 use App\Http\Controllers\ContactController;
 
-//Contacts
-//Read
-Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
-Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
-//Create, Update, Delete
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-Route::patch('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
-Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    //Contacts
+    //Read
+    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+    Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
+    Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+    //Create, Update, Delete
+    Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+    Route::patch('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
+    Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+});
--- routes/projects.php
+++ routes/projects.php
@@ -1,13 +1,16 @@
 <?php
+
 use App\Http\Controllers\ProjectController;
 
-//Projects
-//Read
-Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
-Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
-Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
-Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-//Create, Update, Delete
-Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
-Route::patch('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
-Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+    //Projects
+    //Read
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+    Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+    Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    //Create, Update, Delete
+    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+    Route::patch('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+});
