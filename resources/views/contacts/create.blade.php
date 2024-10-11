b3ceb6f80abdc6bbe8edcd168019c3ab0db981b9
Dominique Vilain <dominique.vilain@hepl.be>
2024-10-11 16:29:14 +0200

    update contact creation form

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -4,59 +4,23 @@
           method="post"
           class="flex flex-col gap-8 bg-slate-50 p-4">
         @csrf
-        <div class="flex flex-col gap-2">
-            <label for="first_name"
-                   class="font-bold first-letter:capitalize">{!! __('first name') !!}
-                <span class="block font-normal">{!! __('min 3 chars, max 255') !!}</span>
-                @error('first_name')
-                <span class="block text-red-500">{!! $message !!}</span>
-                @enderror
-            </label>
-            <input id="first_name"
-                   type="text"
-                   value="{{ old('first_name') }}"
-                   name="first_name"
-                   placeholder="{!! __('jon') !!}"
-                   autocapitalize="none"
-                   autocorrect="off"
-                   spellcheck="false"
-                   class="border border-grey-700 focus:invalid:border-pink-500 invalid:text-pink-600 rounded-md p-2">
-        </div>
-        <div class="flex flex-col gap-2">
-            <label for="last_name"
-                   class="font-bold first-letter:capitalize">{!! __('last name') !!}
-                <span class="block font-normal">{!! __('min 3 chars, max 255') !!}</span>
-                @error('last_name')
-                <span class="block text-red-500">{!! $message !!}</span>
-                @enderror
-            </label>
-            <input id="last_name"
-                   type="text"
-                   value="{{ old('last_name') }}"
-                   name="last_name"
-                   placeholder="{!! __('doe') !!}"
-                   autocapitalize="none"
-                   autocorrect="off"
-                   spellcheck="false"
-                   class="border border-grey-700 focus:invalid:border-pink-500 invalid:text-pink-600 rounded-md p-2">
-        </div>
-        <div class="flex flex-col gap-2">
-            <label for="email"
-                   class="font-bold first-letter:capitalize">{!! __('email of the contact') !!}
-                @error('email')
-                <span class="block text-red-500">{!! $message !!}</span>
-                @enderror
-            </label>
-            <input id="email"
-                   type="text"
-                   value="{{ old('email') }}"
-                   name="email"
-                   placeholder="{!! __('jon@doe.com') !!}"
-                   autocapitalize="none"
-                   autocorrect="off"
-                   spellcheck="false"
-                   class="border border-grey-700 focus:invalid:border-pink-500 invalid:text-pink-600 rounded-md p-2">
-        </div>
+        <x-input-with-label id="first_name"
+                            name="first_name"
+                            label-text="first name"
+                            help-text="min 3 chars, max 255"
+                            placeholder="jon"
+                            :value="old('first_name')"/>
+        <x-input-with-label id="last_name"
+                            name="last_name"
+                            label-text="last name"
+                            help-text="min 3 chars, max 255"
+                            placeholder="doe"
+                            :value="old('last_name')"/>
+        <x-input-with-label id="email"
+                            name="email"
+                            label-text="email of the contact"
+                            placeholder="jon@doe.com"
+                            :value="old('email')"/>
         <x-form-submission-button class="bg-blue-500">{!! __('create this contact') !!}</x-form-submission-button>
     </form>
 </x-app-layout>
