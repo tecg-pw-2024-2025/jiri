d861a31f53341fb921aca1142d57a5594141a72e
Dominique Vilain <dominique.vilain@hepl.be>
2024-10-13 17:15:40 +0200

    add email under contact name
    If two contacts have the same name, it helps to differentiate

--- resources/views/components/contacts/list.blade.php
+++ resources/views/components/contacts/list.blade.php
@@ -2,6 +2,7 @@
     @foreach ($contacts as $contact)
         <li>
             <x-link :route="route('contacts.show',$contact)">{{ $contact->full_name }}</x-link>
+            <p class="text-gray-500">{{  $contact->email }}</p>
         </li>
     @endforeach
 </ol>
