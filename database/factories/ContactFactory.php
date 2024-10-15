1c62cb9ba9683c6669d4a8742b65128cae5d1697
Dominique Vilain <dominique.vilain@hepl.be>
2024-10-15 16:40:11 +0200

    update contact index view and improve contact factory
    contact factory now uses a set of various images

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\Contact;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Storage;
@@ -11,6 +10,17 @@ use Illuminate\Support\Facades\Storage;
 class ContactFactory extends Factory
 {
     protected $model = Contact::class;
+
+    private static array $originals;
+
+    private function getPhotoPath(): string
+    {
+        if (! isset(self::$originals)) {
+            self::$originals = Storage::disk('public')->files('contacts/seed/original');
+        }
+
+        return self::$originals[array_rand(self::$originals)];
+    }
 
     public function definition(): array
     {
@@ -20,7 +30,7 @@ class ContactFactory extends Factory
             'first_name' => $this->faker->firstName(),
             'last_name' => $this->faker->lastName(),
             'email' => $this->faker->unique()->safeEmail(),
-            'photo' => 'contacts/seed/original/gH1wGzeMTZINRbFHkUtxuHtwHrwoKOr0Llwocg5U.jpg',
+            'photo' => $this->getPhotoPath(),
         ];
     }
 }
--- resources/views/components/contacts/list.blade.php
+++ resources/views/components/contacts/list.blade.php
@@ -1,15 +1,16 @@
-<ol class="flex flex-wrap gap-6">
+<ol class="flex flex-wrap gap-6 justify-center">
     @foreach ($contacts as $contact)
-        <li class="flex flex-col gap-2 p-4 bg-slate-50 rounded">
-            <div>
+        <li class="flex flex-col gap-2 p-4 bg-slate-50 rounded-2xl">
+            @php $dimensions = config('photos.sizes.cover') @endphp
+            <div class="leading-0 rounded-[50%] p-4 w-[{{$dimensions*0.9}}px] h-[{{$dimensions*0.9}}px] flex justify-center items-center">
                 <img src="{{ asset(str_replace('original', 'cover', $contact->photo)) }}"
                      alt=""
-                     width="{{ config('photos.sizes.cover') }}"
-                     height="{{ config('photos.sizes.cover') }}"
+                     class="drop-shadow-md rounded-[50%]"
+                     width="{{ $dimensions*0.9 }}"
+                     height="{{ $dimensions*0.9 }}"
                 >
             </div>
             <x-link class="text-center font-bold" :route="route('contacts.show',$contact)">{{ $contact->full_name }}</x-link>
-            <p class="text-gray-500 text-center">{{  $contact->email }}</p>
         </li>
     @endforeach
 </ol>
