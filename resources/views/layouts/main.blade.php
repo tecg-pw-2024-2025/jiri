0c734ce903e14df1bd156cf66fd3c79f0a81a4db
Dominique Vilain <dominique.vilain@hepl.be>
2024-11-02 18:18:28 +0100

    fix responsive width of main content at small sizes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,7 +14,7 @@
 <a class="sr-only skip"
    href="#main-menu">{!! __('go to main menu') !!}</a>
 <div class="flex flex-col gap-6 content items-center w-full">
-    <main class="flex flex-col gap-4 px-4 order-2 lg:w-2/3 md:w-3/4 xl:w-1/2">
+    <main class="flex flex-col gap-4 px-4 order-2 w-full md:w-3/4 lg:w-2/3 xl:w-1/2">
         {{ $slot }}
     </main>
     {{ $navigation }}
